1c8d1cbb480697238d570ccf8a437ed4ca9b3b74
Magdeline Huang <54191678+xsol05@users.noreply.github.com>
2022-08-26 10:26:09 +1200

    Ex 5

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,6 +4,8 @@
 import * as React from 'react'
 import '../box-styles.css'
 
+// Exercise
+
 // 🐨 add a className prop to each div and apply the correct class names
 // based on the text content
 // 💰 Here are the available class names: box, box--large, box--medium, box--small
@@ -14,16 +16,104 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+// const smallBox = (
+//   <div
+//     className="box box--small"
+//     style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
+//   >
+//     small lightblue box
+//   </div>
+// )
+// const mediumBox = (
+//   <div
+//     className="box box--medium"
+//     style={{backgroundColor: 'pink', fontStyle: 'italic'}}
+//   >
+//     medium pink box
+//   </div>
+// )
+// const largeBox = (
+//   <div
+//     className="box box--large"
+//     style={{backgroundColor: 'orange', fontStyle: 'italic'}}
+//   >
+//     large orange box
+//   </div>
+// )
+
+// function App() {
+//   return (
+//     <div>
+//       {smallBox}
+//       {mediumBox}
+//       {largeBox}
+//     </div>
+//   )
+// }
+
+// export default App
+
+// ---
+
+// Extra Credit 1
+// const Box = ({className = '', style, ...children}) => (
+//   <div
+//     className={`box ${className}`.trim()}
+//     style={{fontStyle: 'italic', ...style}}
+//     {...children}
+//   />
+// )
+
+// function App() {
+//   return (
+//     <div>
+//       <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+//         small lightblue box EC1
+//       </Box>
+//       <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+//         medium pink box EC1
+//       </Box>
+//       <Box className="box--large" style={{backgroundColor: 'orange'}}>
+//         large orange box EC1
+//       </Box>
+//       <Box>sizeless box</Box>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// ---
+
+// Extra Credit 2
+const Box = ({size, style, className = '', ...children}) => {
+  const sizeClassName = size ? `box--${size}` : ``
+  return (
+    <div
+      style={{fontStyle: 'italic', ...style}}
+      className={`box ${sizeClassName} ${className}`.trim()}
+      {...children}
+    />
+  )
+}
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box
+        size="small"
+        style={{backgroundColor: 'lightblue'}}
+        className="underline"
+      >
+        small lightblue box EC2
+      </Box>
+      <Box size="medium" style={{backgroundColor: 'pink'}}>
+        medium pink box EC2
+      </Box>
+      <Box size="large" style={{backgroundColor: 'orange'}}>
+        large orange box EC2
+      </Box>
+      <Box>sizeless box</Box>
     </div>
   )
 }
